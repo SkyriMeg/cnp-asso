422f0e087a109cf914d8280e25051f9b0ba47687
SkyriMeg <m-pilon@orange.fr  git config --global user.name SkyriMeggit config --global user.name Meggit config --global user.email m-pilon@orange.fr  git config --global user.email m-pilon@orange.fr>
2025-06-30 13:15:02 +0200

    Project basics

---
+++ assets/greet.js
@@ -0,0 +1,10 @@
+const sayHello = (name) => {
+    return 'Hello ' + name + ', welcome !';
+};
+
+const sayGoodbye = (name) => {
+    return 'Goodbye ' + name;
+}
+
+export default sayHello;
+export { sayGoodbye };
